d469af3fc77b371faa1f6bf6e29de73b6ab76a16
Brian <brian@Brians-MacBook-Pro.local>
2017-01-15 20:43:57 +0800

    Add this.loc to update function

--- js/app.js
+++ js/app.js
@@ -14,7 +14,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    this.loc = loc*dt ;
+    this.loc = this.loc*dt ;
 };
 
 // Draw the enemy on the screen, required method for game
