ab0baa57e715491af01019d3c67420799d8d0cc3
Brian <brian@Brians-MacBook-Pro.local>
2017-01-15 20:23:17 +0800

    add instances for all Enemy array

--- js/app.js
+++ js/app.js
@@ -14,6 +14,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+
 };
 
 // Draw the enemy on the screen, required method for game
@@ -32,7 +33,7 @@ Enemy.prototype.render = function() {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-var allEnemies = [];
+var allEnemies = [new Enemy(1), new Enemy(-50), new Enemy (50)];
 
 
 // This listens for key presses and sends the keys to your
